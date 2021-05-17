3eb4e1570caac6d5dc5055621167ede5460d0213
tugot17 <piotr635@gmail.com>
2021-05-17 17:52:40 +0200

    Path to app added

--- cml_scripts/train_model.py
+++ cml_scripts/train_model.py
@@ -4,10 +4,10 @@ from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 
 from pathlib import Path
-sys.path.append(Path(__file__).parent)
+sys.path.append(str(Path(__file__).parent.parent))
 
 from app.src.model import TransformerModel, bert_config
-from app.src.datamodule import SteamDataloader
+# from app.src.datamodule import SteamDataloader
 
 
 
@@ -21,19 +21,20 @@ TRAIN_RATIO = 0.9
 
 
 if __name__ == "__main__":
-    seed_everything(42)
-
-    dm = SteamDataloader(SEQUENCES_PATH, TRAIN_RATIO, BATCH_SIZE, NUM_WORKERS)
-
-    model = TransformerModel(bert_config)
-    trainer = Trainer(
-        default_root_dir=CHECKPOINTS_DIR,
-        max_epochs=1,
-        gpus=1,
-        deterministic=True,
-        accumulate_grad_batches=2,
-        # callbacks=[EarlyStopping(monitor="val/loss")]
-    )
-
-    # Train model
-    trainer.fit(model, dm)
+    print(sys.path)
+    # seed_everything(42)
+    #
+    # dm = SteamDataloader(SEQUENCES_PATH, TRAIN_RATIO, BATCH_SIZE, NUM_WORKERS)
+    #
+    # model = TransformerModel(bert_config)
+    # trainer = Trainer(
+    #     default_root_dir=CHECKPOINTS_DIR,
+    #     max_epochs=1,
+    #     gpus=1,
+    #     deterministic=True,
+    #     accumulate_grad_batches=2,
+    #     # callbacks=[EarlyStopping(monitor="val/loss")]
+    # )
+    #
+    # # Train model
+    # trainer.fit(model, dm)
