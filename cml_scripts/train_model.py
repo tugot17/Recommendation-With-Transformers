3bf1935d151ba3f0dc9121a1b66c074bc5f808b9
tugot17 <piotr635@gmail.com>
2021-05-17 18:05:21 +0200

    Returned to old approach

--- cml_scripts/train_model.py
+++ cml_scripts/train_model.py
@@ -1,15 +1,10 @@
 import os
-import sys
+
 from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 
-from pathlib import Path
-sys.path.append(str(Path(__file__).parent.parent))
-
 from app.src.model import TransformerModel, bert_config
 from app.src.datamodule import SteamDataloader
-
-
 
 REPO_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 DATA_DIR = os.path.join(REPO_ROOT, "data")
@@ -28,7 +23,7 @@ if __name__ == "__main__":
     model = TransformerModel(bert_config)
     trainer = Trainer(
         default_root_dir=CHECKPOINTS_DIR,
-        max_epochs=1,
+        max_epochs=40,
         gpus=1,
         deterministic=True,
         accumulate_grad_batches=2,
