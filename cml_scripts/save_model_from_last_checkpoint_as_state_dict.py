62436f927b20a5c42533199eec00065dff49d067
tugot17 <piotr635@gmail.com>
2021-05-18 12:36:59 +0200

    Model loading updated

--- cml_scripts/save_model_from_last_checkpoint_as_state_dict.py
+++ cml_scripts/save_model_from_last_checkpoint_as_state_dict.py
@@ -1,11 +1,7 @@
-import re
 from pathlib import Path
 
 import torch
 import yaml
-from pytorch_lightning import Trainer
-
-from app.src.datamodule import SteamDataloader
 from app.src.model import TransformerModel, bert_config
 
 with open("params.yaml", "r") as fd:
@@ -27,28 +23,10 @@ def save_model_from_last_checkpoint_as_state_dict(checkpoints_dir: Path) -> None
     list_of_checkpoints = checkpoints_dir.glob("*.ckpt")
 
     latest_checkpoint_path = max(list_of_checkpoints, key=lambda p: p.stat().st_ctime)
-
-    epoch_number = re.findall(r"\d+", latest_checkpoint_path.name)[0]
-    epoch_number = int(epoch_number)
-
     lightning_model = TransformerModel(bert_config)
-    datamodule = SteamDataloader(
-        SEQUENCES_PATH, TRAIN_SIZE_RATIO, BATCH_SIZE, NUM_WORKERS
-    )
 
-    best_model_trainer = Trainer(
-        max_epochs=epoch_number + 1,
-        gpus=1,
-        deterministic=True,
-        accumulate_grad_batches=4,
-        checkpoint_callback=None,
-        resume_from_checkpoint=latest_checkpoint_path,
-        logger=None,
-    )
-    best_model_trainer.fit(lightning_model, datamodule)
-
+    lightning_model.load_from_checkpoint(latest_checkpoint_path)
     lightning_model.eval()
-
     lightning_model = lightning_model.cpu()
 
     torch.save(lightning_model.backbone.state_dict(), params["best_model_save_path"])
