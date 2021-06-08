4065254baaa0dbee56a31f8f9664733d4e23417c
Daria Puchalska <puchalska.daria@wp.pl>
2021-06-08 20:35:55 +0200

    Add baseline model (Jaccard model) implementation

---
+++ cml_scripts/evaluate_baseline_metrics.py
@@ -0,0 +1,28 @@
+import yaml
+from pathlib import Path
+from time import time
+import json
+
+from app.src.baseline import JaccardModel
+
+with open("params.yaml", "r") as fd:
+    params = yaml.safe_load(fd)
+
+TRAIN_SIZE_RATIO = params["train"]["train_size_ratio"]
+N_NEIGHBORS = params["baseline"]["n_neighbors"]
+
+ROOT_DIR = Path(__file__).parent.parent
+DATA_DIR = ROOT_DIR.joinpath("data")
+CML_DIR = ROOT_DIR.joinpath("cml_scripts")
+SEQUENCES_PATH = DATA_DIR.joinpath("sequences.pickle")
+RESULTS_PATH = CML_DIR.joinpath("baseline_metrics.json")
+
+if __name__ == "__main__":
+    baseline = JaccardModel(SEQUENCES_PATH, TRAIN_SIZE_RATIO, N_NEIGHBORS)
+    start = time()
+    ndcg, mAP = baseline.validate()
+    end = time()
+    validation_time = end - start
+    results = {'time': validation_time, 'mAP': mAP, 'NDCG': ndcg}
+    with open(RESULTS_PATH, "w") as fd:
+        json.dump(results, fd, indent=4)
