2b0c04953bf5c1ade3d38025c1e3a9d9b1a81620
tugot17 <piotr635@gmail.com>
2021-06-15 15:24:18 +0200

    API added

--- api/app/main.py
+++ api/app/main.py
@@ -1,29 +1,87 @@
-from fastapi import FastAPI
-import uvicorn
-from starlette.responses import RedirectResponse
+from typing import List
 
-from app.endpoints import bert_recommendation_endpoint
+import pandas as pd
+from pathlib import Path
+import uvicorn
+from fastapi import FastAPI, Query
+from starlette.middleware.cors import CORSMiddleware
+from model import TransformerModel, bert_config, CLS_TOKEN
+import torch
+
+import os
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
+
+NUMBER_OF_RECOMMENDATIONS = 10
+
+df = pd.read_csv(Path(__file__).parent.joinpath("appids.csv"))
+TRANSFORMER_MODEL_PATH = (
+    Path(__file__)
+    .parent.parent.parent.joinpath("model_state_dicts")
+    .joinpath("bert_model.pth")
+)
+
+app_to_game_dict = dict(zip(df.appid, df.gameid))
+game_to_app_dict = dict(zip(df.gameid, df.appid))
+
+model = TransformerModel(bert_config)
+model.load_state_dict(torch.load(TRANSFORMER_MODEL_PATH))
+model.eval()
 
 
 def create_app():
     app = FastAPI()
-    app.include_router(
-        bert_recommendation_endpoint.router,
-        tags=["model_1_tags"],
-        responses={404: {"description": "Not found"}},
+    # origins = [
+    #     "http://localhost:3001",
+    #     "https://tugot17.github.io/Recommendation-With-Transformers/",
+    # ]
+
+    origins = ["*"]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
+
     return app
 
 
 app = create_app()
 
 
-@app.get("/")
-async def main():
-    url = "/docs"
-    response = RedirectResponse(url=url)
-    return response
+@app.post("/")
+def main(app_ids: List[int] = Query([])) -> List[int]:
+    """
+    Json in: [1, 2, 3]
+    Json out: [5, 6, 7]
+    """
+
+    oiginal_game_ids = list(map(lambda app_id: app_to_game_dict[app_id], app_ids))
+    game_ids = [CLS_TOKEN] + oiginal_game_ids
+    game_ids = torch.tensor(game_ids)
+    game_ids = torch.unsqueeze(game_ids, 0)
+
+    with torch.no_grad():
+        output = model(game_ids)
+
+    # remove games already possessed by a user
+    output[:, oiginal_game_ids] = -float("inf")
+
+    best_games_for_user = (
+        torch.argsort(output, descending=True)[:, :NUMBER_OF_RECOMMENDATIONS]
+        .flatten()
+        .tolist()
+    )
+    best_games_for_user = list(
+        map(lambda game_id: game_to_app_dict[game_id], best_games_for_user)
+    )
+
+    return best_games_for_user
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8011)
+    # print(main([237570, 65560, 24610, 8230, 254000, 8250, 98400]))
+    uvicorn.run(app, host="localhost", port=3001)
--- api/app/model.py
+++ api/app/model.py
@@ -1,10 +1,12 @@
 import pytorch_lightning as pl
 import torch
-from torch.optim import Adam
 from transformers import BertConfig, BertModel
 
-from app.src import CLS_TOKEN, MASK_TOKEN, MAXLEN, NUM_GAMES, PAD_TOKEN
-from app.src.metrics import NDCGMetric, MAPMetric
+
+NUM_GAMES = 2691
+PAD_TOKEN = NUM_GAMES
+MASK_TOKEN = NUM_GAMES + 1
+CLS_TOKEN = NUM_GAMES + 2
 
 HIDDEN_SIZE = 256
 
@@ -17,55 +19,14 @@ bert_config = BertConfig(
 )
 
 
-def loss_fcn(x, positive, negative, output):
-    bpr_loss = 0
-    for i in range(x.shape[0]):
-        pos = output[i][positive[i]]
-        neg = output[i][negative[i]]
-        x_ij = pos.unsqueeze(-1) - neg
-        bpr_loss += -torch.log(torch.sigmoid(x_ij)).sum()
-    bpr_loss = bpr_loss / x.shape[0]
-    return bpr_loss
-
-
 class TransformerModel(pl.LightningModule):
-    lr = 1e-5
-
     def __init__(self, config):
         super().__init__()
         self.save_hyperparameters()
 
         self.model = BertModel(config)
         self.dense = torch.nn.Linear(HIDDEN_SIZE, NUM_GAMES)
-        self.ndcg = NDCGMetric(k=20)
-        self.map = MAPMetric()
 
     def forward(self, x, position_ids=None):
         embedding = self.model(x, position_ids=position_ids).pooler_output
         return self.dense(embedding)
-
-    def configure_optimizers(self):
-        optimizer = Adam(self.parameters(), self.lr)
-        return optimizer
-
-    def training_step(self, batch, batch_idx):
-        x, positive, negative = batch
-        output = self.forward(x, position_ids=torch.zeros_like(x))
-        loss = loss_fcn(x, positive, negative, output)
-
-        self.log("train/loss", loss.item())
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        x, positive, negative = batch
-        output = self.forward(x, position_ids=torch.zeros_like(x))
-        loss = loss_fcn(x, positive, negative, output)
-
-        self.log("val/loss", loss.item())
-
-        self.ndcg(positive, negative, output)
-        self.log("val/ndcg", self.ndcg, on_step=True, on_epoch=True)
-        self.map(positive, negative, output)
-        self.log("val/map", self.map, on_step=True, on_epoch=True)
-
-        return loss
